207874a3594ff7ba2fceb597207736381e2c6879
Glitch (hello-express) <none>
2020-10-09 02:29:34 +0000

    ✂️❤️ Checkpoint ./server.js:23571578/271

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
 
+
+//need a database
+
+//need cache for spotify stuff???
+
+//
 // our default array of dreams
 const dreams = [
   "Find and count some sheep",
